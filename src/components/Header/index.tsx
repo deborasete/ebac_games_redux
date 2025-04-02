d188037d6f54c8e852faa0cdac0b167859ac6380
Débora Sete Acosta <sete.debora@gmail.com>
2025-04-01 21:26:45 -0300

    implements shopping cart with Redux

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,24 @@
+import { useSelector } from 'react-redux'
 import * as S from './styles'
-
-import { Game } from '../../App'
-
 import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
+import { RootReducer } from '../../store'
 
-type Props = {
-  itensNoCarrinho: Game[]
-}
+const Header = () => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
 
-const Header = ({ itensNoCarrinho }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+  const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
 
   return (
     <S.Header>
-      <h1>EBAC Games</h1>
+      <h1>Game Storm</h1>
       <div>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
